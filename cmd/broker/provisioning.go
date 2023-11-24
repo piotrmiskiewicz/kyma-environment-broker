1482f14f761b13576397e3246da1e03f20343d37
Miskiewicz <piotr.miskiewicz@sap.com>
2023-11-24 08:48:00 +0100

    GrardenerCluster deletion step

--- cmd/broker/provisioning.go
+++ cmd/broker/provisioning.go
@@ -39,6 +39,7 @@ func NewProvisioningProcessingQueue(ctx context.Context, provisionManager *proce
 			Once the stage is done it will never be retried.
 	*/
 
+<<<<<<< Updated upstream
 	provisioningSteps := []struct {
 		disabled  bool
 		stage     string
@@ -165,6 +166,132 @@ func NewProvisioningProcessingQueue(ctx context.Context, provisionManager *proce
 			}
 		}
 	}
+=======
+    provisioningSteps := []struct {
+        disabled  bool
+        stage     string
+        step      process.Step
+        condition process.StepCondition
+    }{
+        {
+            stage: startStageName,
+            step:  provisioning.NewStartStep(db.Operations(), db.Instances()),
+        },
+        {
+            stage: createRuntimeStageName,
+            step:  provisioning.NewInitialisationStep(db.Operations(), db.Instances(), inputFactory, runtimeVerConfigurator),
+        },
+        {
+            stage: createRuntimeStageName,
+            step:  steps.NewInitKymaTemplate(db.Operations()),
+        },
+        {
+            stage: createRuntimeStageName,
+            step:  provisioning.NewOverrideKymaModules(db.Operations()),
+        },
+        {
+            stage:     createRuntimeStageName,
+            step:      provisioning.NewResolveCredentialsStep(db.Operations(), accountProvider),
+            condition: provisioning.SkipForOwnClusterPlan,
+        },
+        {
+            stage:    createRuntimeStageName,
+            step:     provisioning.NewInternalEvaluationStep(avsDel, internalEvalAssistant),
+            disabled: cfg.Avs.Disabled,
+        },
+        {
+            stage:     createRuntimeStageName,
+            step:      provisioning.NewEDPRegistrationStep(db.Operations(), edpClient, cfg.EDP),
+            disabled:  cfg.EDP.Disabled,
+            condition: provisioning.SkipForOwnClusterPlan,
+        },
+        {
+            stage: createRuntimeStageName,
+            step:  provisioning.NewOverridesFromSecretsAndConfigStep(db.Operations(), runtimeOverrides, runtimeVerConfigurator),
+            // Preview plan does not call Reconciler so it does not need overrides
+            condition: skipForPreviewPlan,
+        },
+        {
+            condition: provisioning.WhenBTPOperatorCredentialsProvided,
+            stage:     createRuntimeStageName,
+            step:      provisioning.NewBTPOperatorOverridesStep(db.Operations()),
+        },
+        {
+            condition: provisioning.SkipForOwnClusterPlan,
+            stage:     createRuntimeStageName,
+            step:      provisioning.NewCreateRuntimeWithoutKymaStep(db.Operations(), db.RuntimeStates(), db.Instances(), provisionerClient),
+        },
+        {
+            condition: provisioning.DoForOwnClusterPlanOnly,
+            stage:     createRuntimeStageName,
+            step:      provisioning.NewCreateRuntimeForOwnClusterStep(db.Operations(), db.Instances()),
+        },
+        {
+            stage:     createRuntimeStageName,
+            step:      provisioning.NewCheckRuntimeStep(db.Operations(), provisionerClient, cfg.Provisioner.ProvisioningTimeout),
+            condition: provisioning.SkipForOwnClusterPlan,
+        },
+        {
+            stage:    createRuntimeStageName,
+            disabled: cfg.InfrastructureManagerIntegrationDisabled,
+            step:     steps.NewSyncGardenerCluster(db.Operations(), cli),
+        },
+        {
+            stage:    createRuntimeStageName,
+            disabled: cfg.InfrastructureManagerIntegrationDisabled,
+            step:     steps.NewCheckGardenerCluster(db.Operations(), cli),
+        },
+        {
+            stage: createRuntimeStageName,
+            step:  provisioning.NewGetKubeconfigStep(db.Operations(), provisionerClient, k8sClientProvider),
+        },
+        {
+            condition: provisioning.WhenBTPOperatorCredentialsProvided,
+            stage:     createRuntimeStageName,
+            step:      provisioning.NewInjectBTPOperatorCredentialsStep(db.Operations(), k8sClientProvider),
+        },
+        {
+            disabled: cfg.LifecycleManagerIntegrationDisabled,
+            stage:    createRuntimeStageName,
+            step:     steps.SyncKubeconfig(db.Operations(), cli),
+        },
+        {
+            disabled:  cfg.ReconcilerIntegrationDisabled,
+            stage:     createRuntimeStageName,
+            step:      provisioning.NewCreateClusterConfiguration(db.Operations(), db.RuntimeStates(), reconcilerClient),
+            condition: skipForPreviewPlan,
+        },
+        {
+            disabled:  cfg.ReconcilerIntegrationDisabled,
+            stage:     checkKymaStageName,
+            step:      provisioning.NewCheckClusterConfigurationStep(db.Operations(), reconcilerClient, cfg.Reconciler.ProvisioningTimeout),
+            condition: skipForPreviewPlan,
+        },
+        {
+            disabled: cfg.LifecycleManagerIntegrationDisabled,
+            stage:    createKymaResourceStageName,
+            step:     provisioning.NewApplyKymaStep(db.Operations(), cli),
+        },
+        // post actions
+        {
+            stage: postActionsStageName,
+            step:  provisioning.NewExternalEvalStep(externalEvalCreator),
+        },
+        {
+            stage:    postActionsStageName,
+            step:     provisioning.NewInternalEvaluationStep(avsDel, internalEvalAssistant),
+            disabled: cfg.Avs.Disabled,
+        },
+    }
+    for _, step := range provisioningSteps {
+        if !step.disabled {
+            err := provisionManager.AddStep(step.stage, step.step, step.condition)
+            if err != nil {
+                fatalOnError(err)
+            }
+        }
+    }
+>>>>>>> Stashed changes
 
 	queue := process.NewQueue(provisionManager, logs)
 	queue.Run(ctx.Done(), workersAmount)
---
+++ internal/process/deprovisioning/check_gardenercluster_deleted_step.go
@@ -0,0 +1,65 @@
+package deprovisioning
+
+import (
+    "context"
+    "time"
+
+    "github.com/kyma-project/kyma-environment-broker/internal/process/steps"
+
+    "k8s.io/apimachinery/pkg/api/errors"
+    "k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+    "sigs.k8s.io/controller-runtime/pkg/client"
+
+    "github.com/sirupsen/logrus"
+
+    "github.com/kyma-project/kyma-environment-broker/internal"
+    "github.com/kyma-project/kyma-environment-broker/internal/process"
+    "github.com/kyma-project/kyma-environment-broker/internal/storage"
+)
+
+type CheckGardenerClusterDeletedStep struct {
+    operationManager *process.OperationManager
+    kcpClient        client.Client
+}
+
+func NewCheckGardenerClusterDeletedStep(operations storage.Operations, kcpClient client.Client) *CheckGardenerClusterDeletedStep {
+    return &CheckGardenerClusterDeletedStep{
+        operationManager: process.NewOperationManager(operations),
+        kcpClient:        kcpClient,
+    }
+}
+
+func (step *CheckGardenerClusterDeletedStep) Name() string {
+    return "Check_Kyma_Resource_Deleted"
+}
+
+func (step *CheckGardenerClusterDeletedStep) Run(operation internal.Operation, logger logrus.FieldLogger) (internal.Operation, time.Duration, error) {
+    if operation.KymaResourceNamespace == "" {
+        logger.Warnf("namespace for Kyma resource not specified")
+        return operation, 0, nil
+    }
+    resourceName := operation.GardenerClusterName
+    if resourceName == "" {
+        logger.Infof("GardenerCluster resource name is empty, skipping")
+        return operation, 0, nil
+    }
+
+    gardenerClusterUnstructured := &unstructured.Unstructured{}
+    gardenerClusterUnstructured.SetGroupVersionKind(steps.GardenerClusterGVK())
+    err := step.kcpClient.Get(context.Background(), client.ObjectKey{
+        Namespace: operation.KymaResourceNamespace,
+        Name:      resourceName,
+    }, gardenerClusterUnstructured)
+
+    if err == nil {
+        logger.Infof("GardenerCluster resource still exists")
+        return step.operationManager.RetryOperationWithoutFail(operation, step.Name(), "GardenerCluster resource still exists", 5*time.Second, 15*time.Minute, logger)
+    }
+
+    if !errors.IsNotFound(err) {
+        logger.Errorf("unable to check GardenerCluster resource existence: %s", err)
+        return step.operationManager.RetryOperationWithoutFail(operation, step.Name(), "unable to check GardenerCluster resource existence", backoffForK8SOperation, timeoutForK8sOperation, logger)
+    }
+
+    return operation, 0, nil
+}
