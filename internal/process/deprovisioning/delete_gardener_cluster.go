026cab056ca84f0aa12cc9e601bddf540f16fdab
Miskiewicz <piotr.miskiewicz@sap.com>
2023-12-14 15:24:34 +0100

    Fix remove gardenercluster steps

--- internal/process/deprovisioning/delete_gardener_cluster.go
+++ internal/process/deprovisioning/delete_gardener_cluster.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"time"
 
+	"k8s.io/apimachinery/pkg/api/meta"
+
 	"github.com/kyma-project/kyma-environment-broker/internal/process/steps"
 
 	"github.com/kyma-project/kyma-environment-broker/internal"
@@ -54,6 +56,8 @@ func (step *DeleteGardenerClusterStep) Run(operation internal.Operation, logger 
 	if err != nil {
 		if errors.IsNotFound(err) {
 			logger.Info("no GardenerCluster resource to delete - ignoring")
+		} else if meta.IsNoMatchError(err) {
+			logger.Info("No CRD installed, skipping")
 		} else {
 			logger.Errorf("unable to delete the GardenerCluster resource: %s", err)
 			return step.operationManager.RetryOperationWithoutFail(operation, step.Name(), "unable to delete the GardenerCluster resource", backoffForK8SOperation, timeoutForK8sOperation, logger)
