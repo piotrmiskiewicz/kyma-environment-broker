2d487e9b1507f6a285c01410feba7fc7c9e6c68c
Miskiewicz <piotr.miskiewicz@sap.com>
2024-07-08 11:47:22 +0200

    Prepare regions for CN

--- internal/broker/plans.go
+++ internal/broker/plans.go
@@ -154,7 +154,7 @@ func AWSRegions(euRestrictedAccess bool) []string {
 		return []string{"eu-central-1"}
 	}
 	return []string{"eu-central-1", "eu-west-2", "ca-central-1", "sa-east-1", "us-east-1", "us-west-1",
-		"ap-northeast-1", "ap-northeast-2", "ap-south-1", "ap-southeast-1", "ap-southeast-2"}
+		"ap-northeast-1", "ap-northeast-2", "ap-south-1", "ap-southeast-1", "ap-southeast-2", "cn-north-1", "cn-northwest-1"}
 }
 
 func AWSRegionsDisplay() map[string]string {
