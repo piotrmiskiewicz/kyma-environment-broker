e7589c74c0601ee662912a2411ad34eefc23befe
Miskiewicz <piotr.miskiewicz@sap.com>
2024-07-11 12:12:57 +0200

    Test provisioning process with KIM

--- cmd/broker/provisioning_test.go
+++ cmd/broker/provisioning_test.go
@@ -5,13 +5,12 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"github.com/kyma-project/kyma-environment-broker/internal/provider"
+	"github.com/stretchr/testify/require"
 	"io"
 	"net/http"
 	"os"
 	"testing"
-
-	"github.com/kyma-project/kyma-environment-broker/internal/provider"
-	"github.com/stretchr/testify/require"
 
 	"github.com/google/uuid"
 	"github.com/kyma-project/control-plane/components/provisioner/pkg/gqlschema"
@@ -91,6 +90,41 @@ func TestProvisioning_HappyPath(t *testing.T) {
 	suite.WaitForProvisioningState(provisioningOperationID, domain.Succeeded)
 	suite.AssertAllStagesFinished(provisioningOperationID)
 	suite.AssertProvisioningRequest()
+}
+
+func TestProvisioningWithKIM(t *testing.T) {
+	// this test is used for developing KIM integration. The main reason is to just print the Runtime CR as YAML as a result of a process
+
+	cfg := fixConfig()
+	cfg.Broker.KimConfig.Enabled = true
+	cfg.Broker.KimConfig.Plans = []string{"aws", "preview"}
+	cfg.Broker.KimConfig.DryRun = true
+
+	suite := NewBrokerSuiteTestWithConfig(t, cfg)
+	defer suite.TearDown()
+	iid := uuid.New().String()
+	// when
+	resp := suite.CallAPI("PUT", fmt.Sprintf("oauth/v2/service_instances/%s?accepts_incomplete=true", iid),
+		`{
+					"service_id": "47c9dcbf-ff30-448e-ab36-d3bad66ba281",
+					"plan_id": "361c511f-f939-4621-b228-d0fb79a1fe15",
+					"context": {
+						"globalaccount_id": "g-account-id",
+						"subaccount_id": "sub-id",
+						"user_id": "john.smith@email.com"
+					},
+					"parameters": {
+						"name": "testing-cluster",
+						"region": "eu-central-1"
+					}
+		}`)
+
+	opID := suite.DecodeOperationID(resp)
+
+	suite.processProvisioningByOperationID(opID)
+
+	// then
+	suite.WaitForOperationState(opID, domain.Succeeded)
 }
 
 func TestProvisioning_HappyPathAWS(t *testing.T) {
