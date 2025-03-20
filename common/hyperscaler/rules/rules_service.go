3d2580ffc8dc90a35b4b5fe992c44d2488ed49c2
Jarosław Pieszka <jaroslaw.pieszka@sap.com>
2025-03-20 07:51:04 +0100

    Subscription Service - `log.Fatalf` removed (#1879)
    logfatal removed

--- common/hyperscaler/rules/rules_service.go
+++ common/hyperscaler/rules/rules_service.go
@@ -145,7 +145,6 @@ func (rs *RulesService) FirstParsingError() error {
 			})
 
 			printer.Print(rs.ParsedRuleset.Results, nil)
-			log.Fatalf("Parsing errors occurred during rules parsing")
 			return fmt.Errorf("Parsing errors occurred during rules parsing, results are: %s", buffer)
 		}
 	}
