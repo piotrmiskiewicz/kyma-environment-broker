5e5e327cda0afac219f45c846a80b94d7184a056
Miskiewicz <piotr.miskiewicz@sap.com>
2025-11-13 14:00:08 +0100

    Validation

---
+++ scripts/notable_changes_validator.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# This script validates that if there are any files (other than README.md) in the notable-changes-to-release.
+# If yes, we require that notable-changes-to-release/notable-changes.md exists.
+
+files=$(find notable-changes-to-release -type f ! -name 'README.md')
+if [ -n "$files" ]; then
+    # Check if notable-changes.md exists
+    if [ ! -f notable-changes-to-release/notable-changes.md ]; then
+        echo "Validation failed: notable-changes-to-release/notable-changes.md is missing."
+        exit 1
+    fi
+fi
