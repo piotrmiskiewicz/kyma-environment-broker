a6782fd8f3cefafdaf9d2ccacc71ba3ce31dac52
Piotr Miśkiewicz <pmiskiew@wp.pl>
2025-11-14 08:42:19 +0100

    Update scripts/notable_changes_validator.sh
    Co-authored-by: Jarosław Pieszka <jaroslaw.pieszka@sap.com>

--- scripts/notable_changes_validator.sh
+++ scripts/notable_changes_validator.sh
@@ -3,7 +3,7 @@
 # This script validates that if there are any files (other than README.md) in the notable-changes-to-release.
 # If yes, we require that notable-changes-to-release/notable-changes.md exists.
 
-files=$(find notable-changes-to-release -type f ! -name 'README.md')
+files=$(find notable-changes-to-release -type f -not -name 'README.md')
 if [ -n "$files" ]; then
     # Check if notable-changes.md exists
     if [ ! -f notable-changes-to-release/notable-changes.md ]; then
